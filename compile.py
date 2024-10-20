da6d9cebff10dbb89fe97722e5bf233843e1a56f
Manuel Huber <manuel.h87@gmail.com>
2024-10-21 01:18:51 +0200

    Fix alignment issue
    Let the compiler figure out the proper alignment.

--- compile.py
+++ compile.py
@@ -4,6 +4,14 @@ import cffi
 _RX_ATTR = re.compile(r"__attribute__\(.*\)")
 _RX_CMT = re.compile(r"/[*].*[*]/")
 
+_REPL_CAN_FRAME = """
+	canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+	uint8_t    len;     /* frame payload length in byte */
+	uint8_t    flags;   /* additional flags for CAN FD */
+	uint8_t    data[CANFD_MAX_DLEN];
+        ...;
+"""
+
 
 def _filter(line):
     line = _RX_CMT.sub('', line)
@@ -14,10 +22,39 @@ def _filter(line):
     return line
 
 
+def _find_end(data, i):
+    level = 1
+    for c in data[i:]:
+        if c == '{':
+            level += 1
+        elif c == '}':
+            level -= 1
+        if level == 0:
+            return i
+        i += 1
+    raise RuntimeError("Missing end of struct")
+
+
+def _replace_struct(name, data, repl):
+    text = "struct " + name + " {"
+    i = data.find(text)
+    if i < 0:
+        raise RuntimeError(f"Struct {name} not found")
+    i += len(text)
+    pre = data[0:i]
+    end = _find_end(data, i)
+    assert end >= i, "Index sanity check"
+    post = data[end:]
+    return pre + '\n' + repl + '\n' + post
+
+
 ffibuilder = cffi.FFI()
 
 with open('libpycan.h') as f:
     data = ''.join([_filter(line) for line in f])
+    data = _replace_struct('canfd_frame', data, _REPL_CAN_FRAME)
+    with open('_libpycan.python.h', 'w') as out:
+        out.write(data)
     ffibuilder.embedding_api(data)
 
 ffibuilder.set_source("_libpycan", r'''
