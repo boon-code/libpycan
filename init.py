5eec5348cb32ae3c0c94b8fa0ceac0abd7dcfdf5
Manuel Huber <manuel.h87@gmail.com>
2024-10-21 11:29:47 +0200

    Move internal functions up

--- init.py
+++ init.py
@@ -6,6 +6,52 @@ from pathlib import Path
 _bus = None
 
 
+def _to_utf8(c_buffer):
+    return ffi.string(c_buffer).decode('utf-8')
+
+
+def _to_message(buffer):
+    is_ext = ((buffer.can_id & lib.CAN_EFF_FLAG) != 0)
+    is_rtr = ((buffer.can_id & lib.CAN_RTR_FLAG) != 0)
+    is_err = ((buffer.can_id & lib.CAN_ERR_FLAG) != 0)
+    data = buffer.data[0:buffer.len]
+    if is_rtr:
+        data = bytes()
+    can_id = buffer.can_id & lib.CAN_EFF_MASK
+    if not is_ext:
+        can_id = buffer.can_id & lib.CAN_SFF_MASK
+    if is_err:
+        can_id = buffer.can_id & lib.CAN_ERR_MASK
+    return can.Message(
+        arbitration_id=can_id,
+        is_extended_id=is_ext,
+        is_remote_frame=is_rtr,
+        is_error_frame=is_err,
+        dlc=buffer.len,
+        data=data,
+    )
+
+
+def _set_c_frame(msg, buffer):
+    flags = 0
+    if msg.is_extended_id:
+        flags |= lib.CAN_EFF_FLAG
+    if msg.is_remote_frame:
+        flags |= lib.CAN_RTR_FLAG
+    if msg.is_error_frame:
+        flags |= lib.CAN_ERR_FLAG
+    can_id = msg.arbitration_id & lib.CAN_EFF_MASK
+    assert can_id == msg.arbitration_id, "identity"
+    if not msg.is_extended_id:
+        can_id = msg.arbitration_id & lib.CAN_SFF_MASK
+    buffer.can_id = can_id | flags
+    buffer.len = msg.dlc
+    buffer.flags = 0
+    if not msg.is_remote_frame:
+        for i in range(msg.dlc):
+            buffer.data[i] = msg.data[i]
+
+
 @ffi.def_extern()
 def GetVersion(version):
     if version == ffi.NULL:
@@ -29,10 +75,6 @@ def CanInitDefault():
     except:
         return lib.PYCAN_RESULT_FAIL
     return lib.PYCAN_RESULT_OK
-
-
-def _to_utf8(c_buffer):
-    return ffi.string(c_buffer).decode('utf-8')
 
 
 @ffi.def_extern()
@@ -95,48 +137,6 @@ def CanRead(buffer, buffer_size, timeout, n_frames_read):
     if count < buffer_size:
         return lib.PYCAN_RESULT_INCOMPLETE
     return lib.PYCAN_RESULT_OK
-
-
-def _to_message(buffer):
-    is_ext = ((buffer.can_id & lib.CAN_EFF_FLAG) != 0)
-    is_rtr = ((buffer.can_id & lib.CAN_RTR_FLAG) != 0)
-    is_err = ((buffer.can_id & lib.CAN_ERR_FLAG) != 0)
-    data = buffer.data[0:buffer.len]
-    if is_rtr:
-        data = bytes()
-    can_id = buffer.can_id & lib.CAN_EFF_MASK
-    if not is_ext:
-        can_id = buffer.can_id & lib.CAN_SFF_MASK
-    if is_err:
-        can_id = buffer.can_id & lib.CAN_ERR_MASK
-    return can.Message(
-        arbitration_id=can_id,
-        is_extended_id=is_ext,
-        is_remote_frame=is_rtr,
-        is_error_frame=is_err,
-        dlc=buffer.len,
-        data=data,
-    )
-
-
-def _set_c_frame(msg, buffer):
-    flags = 0
-    if msg.is_extended_id:
-        flags |= lib.CAN_EFF_FLAG
-    if msg.is_remote_frame:
-        flags |= lib.CAN_RTR_FLAG
-    if msg.is_error_frame:
-        flags |= lib.CAN_ERR_FLAG
-    can_id = msg.arbitration_id & lib.CAN_EFF_MASK
-    assert can_id == msg.arbitration_id, "identity"
-    if not msg.is_extended_id:
-        can_id = msg.arbitration_id & lib.CAN_SFF_MASK
-    buffer.can_id = can_id | flags
-    buffer.len = msg.dlc
-    buffer.flags = 0
-    if not msg.is_remote_frame:
-        for i in range(msg.dlc):
-            buffer.data[i] = msg.data[i]
 
 
 @ffi.def_extern()
